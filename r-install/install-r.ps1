3dec3387dc8eddfe240d5036f3d8eeb966c6c12f
matthew-townson <mjwtownson@gmail.com>
2024-07-08 16:19:16 +0100

    updated r script

--- r-install/install-r.ps1
+++ r-install/install-r.ps1
@@ -1,5 +1,5 @@
 cd ~/Downloads
-Invoke-RestMethod -Uri www.stats.bris.ac.uk/R/bin/windows/base/R-4.3.3-win.exe -OutFile ~/Downloads/R-installer.exe
-Invoke-RestMethod -Uri download1.rstudio.org/electron/windows/RStudio-2023.12.1-402.exe -OutFile ~/Downloads/RStudio.exe
+Invoke-RestMethod -Uri www.stats.bris.ac.uk/R/bin/windows/base/R-4.4.1-win.exe -OutFile ~/Downloads/R-installer.exe
+Invoke-RestMethod -Uri download1.rstudio.org/electron/windows/RStudio-2024.04.2-764.exe -OutFile ~/Downloads/RStudio.exe
 ./R-installer.exe
 ./RStudio.exe
