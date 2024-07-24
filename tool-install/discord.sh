2fbbac6adee2c97f8972d62d6d02f8a08626a58c
matthew-townson <mjwtownson@gmail.com>
2024-07-24 16:13:56 +0100

    removed wait and added discord
    uses wget to download latest discord release

---
+++ tool-install/discord.sh
@@ -0,0 +1,3 @@
+wget -r 'https://discord.com/api/download/stable?platform=linux&format=deb' -O "./discord.deb"
+sudo apt install ./discord.deb -y
+rm discord.deb
--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sudo ntpdate 0.uk.pool.ntp.org
-wait
 sudo apt update
-wait
 sudo apt upgrade -y
-wait
 sudo apt autoremove -y
